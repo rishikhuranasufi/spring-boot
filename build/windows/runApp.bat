c6c56ea75bc417bde95f3686f69760bdf05bdfaf
Rishi Khurana(rishi.khurana) <rishi.khurana@infosys.com>
2018-09-13 14:02:03 +0530

    Intial commit

---
+++ build/windows/runApp.bat
@@ -0,0 +1,2 @@
+//net user rishi.khurana /active:yes
+sc create sampleApp binpath= "java.exe -jar gs-spring-boot-0.1.0.jar"
---
+++ src/test/java/com/infy/controller/HelloControllerTest.java
@@ -0,0 +1,43 @@
+package com.infy.controller;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+public class HelloControllerTest {
+
+    @Autowired
+    private MockMvc mvc;
+
+
+    @Before
+    public void setUp() {
+        /*mvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(SecurityMockMvcConfigurers.springSecurity())
+                .build();*/
+    }
+    @Test
+    public void getHello() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Welcome to EQS training sample application !!")));
+    }
+}
